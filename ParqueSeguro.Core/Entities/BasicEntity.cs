fac8f2938119f5c1b39b30af3f5db5eaaf769c6c
Gustavo Deola <gustavodeola@gmail.com>
2025-07-01 23:45:37 -0300

    style: Index

--- ParqueSeguro.Core/Entities/BasicEntity.cs
+++ ParqueSeguro.Core/Entities/BasicEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ParqueSeguro.Core.Entities
+﻿namespace ParqueSeguro.Core.Entities
 {
     public abstract class BasicEntity
     {

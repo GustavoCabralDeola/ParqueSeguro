74016d92169c5bb4e6a747a1c5b00c11427bc12f
Gustavo Deola <gustavodeola@gmail.com>
2025-07-02 21:06:54 -0300

    refactor: usings desnecessários

--- ParqueSeguro.Core/Interfaces/Respositories/IRegistroRepository.cs
+++ ParqueSeguro.Core/Interfaces/Respositories/IRegistroRepository.cs
@@ -1,11 +1,5 @@
-﻿using ParqueSeguro.Core.Entities;
-using ParqueSeguro.Core.InputModels;
+﻿using ParqueSeguro.Core.InputModels;
 using ParqueSeguro.Core.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ParqueSeguro.Core.Interfaces.Respositories
 {
--- ParqueSeguro.Core/Interfaces/Respositories/ITabelaPrecoRepository.cs
+++ ParqueSeguro.Core/Interfaces/Respositories/ITabelaPrecoRepository.cs
@@ -1,9 +1,4 @@
 ﻿using ParqueSeguro.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ParqueSeguro.Core.Interfaces.Respositories
 {
